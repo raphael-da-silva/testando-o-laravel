080758032d3bf516ba6eb3aacd0ef8a0918bd975
Raphael da Silva <raphael_correa_silva@hotmail.com>
2020-07-29 21:51:08 -0300

    Criação de página de erro 404 customizada

---
+++ resources/views/404.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+	<div class="jumbotron bg-danger text-white text-center">
+		<h3>
+			<span class="text-warning">404</span> - 
+			A página não existe
+		</h3>
+	</div>
+@endsection
